aa499ee1542d14c6927818c20276ac4820b70384
Marc Lichtman <marclichtman@microsoft.com>
2022-10-08 00:04:46 -0400

    now redirects to home page if you go to a recording page without connection info being filled out

--- iqengine-browse/src/App.js
+++ iqengine-browse/src/App.js
@@ -6,18 +6,16 @@ import React , { Component } from 'react';
 import ConnectionStringInput from './Components/FileBrowser/ConnectionString';
 import JsonDataDisplay from './Components/FileBrowser/JSONDataDisplay';
 import SpectrogramPage from './Components/Spectrogram/SpectrogramPage';
-
 import './Components/Spectrogram/sidebar.css';
-
 import '@fortawesome/react-fontawesome';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import {
   Routes,
   Route,
   Link,
+  Navigate,
 } from "react-router-dom";
+
 
 class App extends Component {
   constructor(props){
@@ -33,38 +31,38 @@ class App extends Component {
     this.setState({recordingList: x});
   }
 
-    render() {
-      return (
-          <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/spectrogram/testttt">About</Link>
-              </li>
-              <li>
-                <Link to="/dashboard">Dashboard</Link>
-              </li>
-            </ul>
-            <hr />
-            
-            <Routes>
-              <Route exact path="/" element={ 
-                <>
-                <div><center><h1 className="display-1"><b>IQEngine</b></h1></center></div>
-                <ConnectionStringInput setRecordingList={this.setRecordingList} />
-                <JsonDataDisplay data={this.state.recordingList}/>
-                </>
-              } />
+  render() {
+    return (
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/spectrogram/testttt">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+          <hr />
+          
+          <Routes>
+            <Route exact path="/" element={ 
+              <>
+              <div><center><h1 className="display-1"><b>IQEngine</b></h1></center></div>
+              <ConnectionStringInput setRecordingList={this.setRecordingList} />
+              <JsonDataDisplay data={this.state.recordingList}/>
+              </>
+            } />
 
-              <Route path="/spectrogram/:recording" element={
-                <SpectrogramPage />
-              } />
-            </Routes>
-          </div>
-      );
-    }
+            <Route path="/spectrogram/:recording" element={
+              this.state.recordingList.length !== 0 ? <SpectrogramPage /> : <Navigate to="/" />
+            } />
+          </Routes>
+        </div>
+    );
   }
+}
 
 export default App;
