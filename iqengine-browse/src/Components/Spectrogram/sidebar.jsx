7c3bf8873d79f2369f4af663fdba7d8e868667c4
Marc Lichtman <marclichtman@microsoft.com>
2022-12-31 23:35:25 -0500

    got rid of leftpanel so there is 1 less hop with all these handlers

--- iqengine-browse/src/Components/Spectrogram/sidebar.jsx
+++ iqengine-browse/src/Components/Spectrogram/sidebar.jsx
@@ -3,20 +3,39 @@
 
 import React from 'react';
 import { Nav } from 'react-bootstrap';
-import LeftPanel from './LeftPanel';
+import SettingsPane from './SettingsPane';
+import InfoPane from './InfoPane';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Sidebar = (props) => {
   //from: https://stackoverflow.com/questions/60482018/make-a-sidebar-from-react-bootstrap
   return (
     <Nav className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
       <Nav.Item>
-        <LeftPanel
-          handleFftSize={props.handleFftSize}
-          handleMagnitudeMax={props.handleMagnitudeMax}
-          handleMagnitudeMin={props.handleMagnitudeMin}
-          updateBlobTaps={props.updateBlobTaps}
-          meta={props.meta}
-        />
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Settings</Accordion.Header>
+            <Accordion.Body>
+              <SettingsPane
+                handleFftSize={props.handleFftSize}
+                handleMagnitudeMax={props.handleMagnitudeMax}
+                handleMagnitudeMin={props.handleMagnitudeMin}
+                updateBlobTaps={props.updateBlobTaps}
+                meta={props.meta}
+              />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Info</Accordion.Header>
+            <Accordion.Body>
+              <InfoPane meta={props.meta} />
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>Annotations</Accordion.Header>
+            <Accordion.Body></Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </Nav.Item>
     </Nav>
   );
